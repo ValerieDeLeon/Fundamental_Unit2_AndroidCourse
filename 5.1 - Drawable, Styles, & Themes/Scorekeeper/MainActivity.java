7bac60665684322bb27cabc27d4216fccac6974f
Valerie <valeriedeleon4521@gmail.com>
2020-02-07 08:54:39 -0800

    Update MainActivity.java

--- 5.1 - Drawable, Styles, & Themes/Scorekeeper/MainActivity.java
+++ 5.1 - Drawable, Styles, & Themes/Scorekeeper/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.scorekeeper;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
@@ -14,6 +17,40 @@ public class MainActivity extends AppCompatActivity {
     private TextView mScoreText2;
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Change the label of the menu based on the state of the app.
+        int nightMode = AppCompatDelegate.getDefaultNightMode();
+        if(nightMode == AppCompatDelegate.MODE_NIGHT_YES){
+            menu.findItem(R.id.night_mode).setTitle(R.string.day_mode);
+        } else{
+            menu.findItem(R.id.night_mode).setTitle(R.string.night_mode);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //Check if the correct item was clicked
+        if(item.getItemId()==R.id.night_mode)
+        {
+            if(item.getItemId()==R.id.night_mode){
+                // Get the night mode state of the app.
+                int nightMode = AppCompatDelegate.getDefaultNightMode();
+                //Set the theme mode for the restarted activity
+                if (nightMode == AppCompatDelegate.MODE_NIGHT_YES) {
+                    AppCompatDelegate.setDefaultNightMode
+                            (AppCompatDelegate.MODE_NIGHT_NO);
+                } else {
+                    AppCompatDelegate.setDefaultNightMode
+                            (AppCompatDelegate.MODE_NIGHT_YES);
+                }
+                // Recreate the activity for the theme change to take effect.
+                recreate();
+        }
+        return true;
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -22,38 +59,38 @@ public class MainActivity extends AppCompatActivity {
         mScoreText2 = findViewById(R.id.score_2);
     }
 
-    public void decreaseScore(View view) {
-        // Get the ID of the button that was clicked
-        int viewID = view.getId();
-        switch (viewID){
-            //If it was on Team 1
-            case R.id.decreaseTeam1:
-                //Decrement the score and update the TextView
-                mScore1--;
-                mScoreText1.setText(String.valueOf(mScore1));
-                break;
-            //If it was Team 2
-            case R.id.decreaseTeam2:
-                //Decrement the score and update the TextView
-                mScore2--;
-                mScoreText2.setText(String.valueOf(mScore2));
+        public void decreaseScore (View view) {
+            // Get the ID of the button that was clicked
+            int viewID = view.getId();
+            switch (viewID) {
+                //If it was on Team 1
+                case R.id.decreaseTeam1:
+                    //Decrement the score and update the TextView
+                    mScore1--;
+                    mScoreText1.setText(String.valueOf(mScore1));
+                    break;
+                //If it was Team 2
+                case R.id.decreaseTeam2:
+                    //Decrement the score and update the TextView
+                    mScore2--;
+                    mScoreText2.setText(String.valueOf(mScore2));
+            }
+        }
+        public void increaseScore (View view){
+            //Get the ID of the button that was clicked
+            int viewID = view.getId();
+            switch (viewID) {
+                //If it was on Team 1
+                case R.id.increaseTeam1:
+                    //Increment the score and update the TextView
+                    mScore1++;
+                    mScoreText1.setText(String.valueOf(mScore1));
+                    break;
+                //If it was Team 2
+                case R.id.increaseTeam2:
+                    //Increment the score and update the TextView
+                    mScore2++;
+                    mScoreText2.setText(String.valueOf(mScore2));
+            }
         }
     }
-    public void increaseScore(View view) {
-        //Get the ID of the button that was clicked
-        int viewID = view.getId();
-        switch (viewID){
-            //If it was on Team 1
-            case R.id.increaseTeam1:
-                //Increment the score and update the TextView
-                mScore1++;
-                mScoreText1.setText(String.valueOf(mScore1));
-                break;
-            //If it was Team 2
-            case R.id.increaseTeam2:
-                //Increment the score and update the TextView
-                mScore2++;
-                mScoreText2.setText(String.valueOf(mScore2));
-        }
-    }
-}
