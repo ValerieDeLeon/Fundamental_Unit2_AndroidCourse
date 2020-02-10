4770d449c5c75e674fe52ebb12e4a4feea1e3673
Valerie <valeriedeleon4521@gmail.com>
2020-02-10 08:34:45 -0800

    Update MainActivity.java

--- 5.1 - Drawable, Styles, & Themes/Scorekeeper/HW - Battery Indictator/MainActivity.java
+++ 5.1 - Drawable, Styles, & Themes/Scorekeeper/HW - Battery Indictator/MainActivity.java
@@ -1,1 +1,14 @@
-hello
+package com.example.batteryindicator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
